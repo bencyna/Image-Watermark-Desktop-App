e7c7e45bbdbd324964eabac1f08501043d5dd094
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-02-06 20:32:03 +1030

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+# ToDo 1. Setup tkinter window
+
+
+# ToDo 2. Allow users to upload an image
+
+
+# ToDo 3. When user clicks on image, add text should come up with options (size, colour, font etc)
+
+
+#ToDo 4. User needs to be able to save the image
